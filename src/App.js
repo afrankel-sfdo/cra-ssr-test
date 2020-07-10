a9218a401493d560027dd007b76e8191af090620
Andrew Frankel <afrankel@salesforce.com>
2020-07-10 08:43:21 -0600

    null coalescing test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload!
         </p> 
         <p>
-        {QUERY.loc?.source.name}
+        {QUERY.loc?.source.name ?? 'undefined'}
         </p>
       </header>
     </div>
