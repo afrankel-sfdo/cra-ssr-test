55e1c14d4c3b7ed8c972d65b690a2b97c423e062
Andrew Frankel <afrankel@salesforce.com>
2020-07-09 20:07:07 -0600

    better query test

--- src/App.js
+++ src/App.js
@@ -4,24 +4,19 @@ import './App.css';
 import gql from 'graphql-tag';
 
 const QUERY = gql`query do{ me { name }}`;
-// console.log('query', QUERY);
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload!!
+          Edit <code>src/App.js</code> and save to reload!
+        </p> 
+        <p>
+        {QUERY.loc.source.name}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
